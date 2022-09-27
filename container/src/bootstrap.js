c3ffa14d5e30f124d92774fe46998ec0e4f25696
Mohammad Arif <arif_iq@yahoo.co.in>
2022-09-27 23:05:04 +0530

    Consume remote modules and refactoring Products and Cart projects

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,9 @@
-import 'products/ProductsIndex'
-import 'cart/CartShow'
+import { mount as productsMount } from 'products/ProductsIndex'
+import { mount as cartMount } from 'cart/CartShow'
 
 console.log('Container!!!')
+
+const products = document.querySelector('#my-products')
+const cart = document.querySelector('#my-cart')
+productsMount(products)
+cartMount(cart)
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,33 @@
 import faker from 'faker'
 
-let products = ''
+const mount = el => {
+  let products = ''
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName()
-  products += `<div>${name}</div>`
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName()
+    products += `<div>${name}</div>`
+  }
+
+  el.innerHTML = products
 }
 
-document.getElementById('dev-products').innerHTML = products
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are suing our local index.html file
+// Which DEFINITELY has and element with an id of 'dev-product'
+// We want to immediately render our app into th at element
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products')
+
+  // Assuming our container doesn't have an element
+  // with the id 'dev-products'
+  if (el) {
+    mount(el) // we are probably running in isolation
+  }
+}
+// Context/Situation #2
+// We are running this file in development/production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products'
+// We DO NOT WANT to try to immediately render the app
+export { mount }
